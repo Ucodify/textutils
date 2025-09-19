19372afd44c151cef7c3d8d8a3696efbd1b9a33c
UCodify <hasasamee@gmail.com>
2025-09-19 09:51:30 +0500

    Props and states

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Home</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Services
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Marketing</a></li>
-            <li><a className="dropdown-item" href="/">Software Solutions</a></li>
-            <li><hr className="dropdown-divider"/>Hiring</li>
-            <li><a className="dropdown-item" href="/">Contact</a></li>
-          </ul>
-        </li>
-        {/* <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li> */}
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+      <div className="container my-3">
+        <Navbar title="TextUtils" aboutText="About TextUtils" />
+        <TextForm heading="Enter the text to analyse" />
+      </div>
+
+      {/* <Navbar /> */}
     </>
-   
   );
 }
 
 export default App;
-
